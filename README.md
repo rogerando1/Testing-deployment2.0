24602c52f1fa13adca467442372175ebd57405c4
Roger R. Ando <139098175+rogerando1@users.noreply.github.com>
2023-12-15 08:26:44 +0800

    changes

--- README.md
+++
@@ -1,1 +0,0 @@
-# Testing-deployment2.0
--- frontend/src/hooks/user_data_atom.js
+++
@@ -1,7 +0,0 @@
-import { atom, useAtom } from 'jotai';
-
-export const userDataAtom = atom({})
-
-export function useUserDataAtom(){
-  return useAtom(userDataAtom)
-}
