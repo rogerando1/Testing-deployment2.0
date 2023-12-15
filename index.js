54f518238e96eeaf1ad3a8db42b7ef8b676829d9
Roger R. Ando <139098175+rogerando1@users.noreply.github.com>
2023-12-15 08:54:31 +0800

    ccc

--- index.js
+++ index.js
@@ -28,18 +28,6 @@ app.use(
   })
 );
 
-
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
-    function (err) {
-      res.status(500).send(err);
-    }
-  );
-});
-
-
 app.use(cookieParser());
 app.use("/uploaded-files", express.static("uploaded-files"));
 app.use("/uploaded-image", express.static("uploaded-image"));
